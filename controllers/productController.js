584cf78c0734b95dc64e3d54f523fe14841465cb
Ramzi Bouzaiene <ramzi.bouzaiene@sastec-group.com>
2023-04-08 23:29:47 +0100

    adding product by id

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,7 @@
 const Product = require("../models/productModel");
 
+// @desc Get All Products
+// @route GET /api/products
 async function getProducts(req, res) {
   try {
     const products = await Product.findAll();
@@ -12,6 +14,25 @@ async function getProducts(req, res) {
   }
 }
 
+// @desc Get One Product
+// @route GET /api/product/:id
+async function getProduct(req, res, id) {
+  try {
+    const product = await Product.findById(id);
+
+    if (!product) {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "Product Not Found" }));
+    } else {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(product));
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 module.exports = {
   getProducts,
+  getProduct,
 };
